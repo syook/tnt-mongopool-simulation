c06f6e102631aa3a5f4ea43b276f2d4a14e2ff37
Aman Agarwal <aman@syook.com>
2018-06-18 16:42:46 +0530

    log compute times and rolling avg, allowDiskUse true

--- index.js
+++ index.js
@@ -6,22 +6,35 @@ const kue = require('kue');
 const queue = kue.createQueue();
 kue.app.listen(8001);
 
+const moment = require('moment');
+
+let avg = 0;
+let i = 0;
+let sum = 0;
+
 queue.on('job enqueue', function(id, type) {
   console.log('Job %s got queued of type %s', id, type);
 });
 
 const processAggregate = () => {
   return new Promise((resolve,reject) => {
-    Card.aggregate([{$group: {_id: null,total: {$sum: "$number"}}}]).exec((err,res) => {
-      if(err) return reject(err);
+    const cardQuery = Card.aggregate([{$group: {_id: null,total: {$sum: "$number"}}}])
+    cardQuery.option({allowDiskUse: true});
+    cardQuery.exec((err, res) => {
+      if (err) return reject(err);
       resolve(res);
-    })
+    });
   });
 };
 
 queue.process('aggregate',32,(job,done) => {
+  let time = moment();
   processAggregate().then(res => {
     console.log(res);
+    i++;
+    sum += Math.round(moment().diff(time) / 1000);
+    avg = sum / i;
+    console.log(`compute time: ${Math.round(moment().diff(time) / 1000)} avg: ${avg}  job id: ${job.id}`);
     done(null,res)
   }).catch(err => done(err));
 });
--- models/db.js
+++ models/db.js
@@ -7,7 +7,7 @@ var options = {
   autoIndex: false, // Don't build indexes
   reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
   reconnectInterval: 500, // Reconnect every 500ms
-  poolSize: 15, // Maintain up to 10 socket connections
+  poolSize: 5, // Maintain up to 10 socket connections
   // If not connected, return errors immediately rather than waiting for reconnect
   bufferMaxEntries: 0
 };
